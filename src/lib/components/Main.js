3867094745454d10f621a25f9acbc327099675f1
lucerohospina <lucerojhp@gmail.com>
2018-03-12 11:19:11 -0500

    adding redux files step-2

--- src/lib/components/Main.js
+++ src/lib/components/Main.js
@@ -6,16 +6,6 @@ import FilterableProductsTable from '../FilterableProductsTable/components';
 
 // toda los datos que Main necesita, ahora los recibimos como props
 const Main = ({ products }) => {
-  // TODO: Hack para que renderice. Quitar luego de setear Redux.
-  products = [
-    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-    { category: 'Electronics', price: '$1666699.99', stocked: true, name: 'Nexus 7' }
-  ];
-
   return (
     <div>
       <FilterableProductsTable products={products} />
